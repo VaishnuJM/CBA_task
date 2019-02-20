ef63c3daae7dece23824ccf5e5186342acaee026
VaishnuJM <32368681+VaishnuJM@users.noreply.github.com>
2019-02-20 12:13:41 +0530

    Create sorting_using_malloc.c

---
+++ sorting_using_malloc.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+int* sortfunc(int *ptr1);
+int main()
+
+{
+    int t,i,j;
+    int *ptr=malloc(5*sizeof(int));
+    int *c;
+    for(i=0;i<5;i++)
+    {
+       scanf("%d",(ptr+i));
+    }
+    c=sortfunc(ptr);
+    for(i=0;i<5;i++)
+    {
+        printf("%d ",*(c+i));
+    }
+    return 0;
+}
+int *sortfunc(int *ptr1)
+{
+    int i,j,t;
+    for(i=0;i<5;i++)
+    {
+        for(j=i+1;j<5;j++)
+        {
+            if(*(ptr1+i)< *(ptr1+j))
+            {
+                t=*(ptr1+i);
+                *(ptr1+i)= *(ptr1+j);
+                *(ptr1+j)=t;
+            }
+        }
+    }
+ return ptr1;
+}
